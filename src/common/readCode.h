c5f14577f6ff50a80cb212660da99d0bae9d4b66
Waloid24 <111529670+Waloid24@users.noreply.github.com>
2023-01-31 20:44:52 +0300

    Updated program structure

---
+++ src/common/readCode.h
@@ -0,0 +1,22 @@
+#ifndef READ_CODE_H
+#define READ_CODE_H
+
+#include <stdio.h>
+#include <sys/stat.h>
+#include "../debug/MY_ASSERT.h"
+
+struct codeInfo
+{
+    size_t sizeFile;
+    char * buf;
+    size_t nStrs;
+    char ** arrStrs;
+};
+typedef struct codeInfo code_t;
+
+code_t readCode (const char * nameFile);
+void * allocateMemory (size_t size, size_t sizeElem);
+FILE * openFile(const char * nameFile, const char * format);
+char * getCode (const char * nameFile);
+
+#endif
